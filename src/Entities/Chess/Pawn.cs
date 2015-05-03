cc84024a51480460f242a8b27d20e8066d0a43aa
Andrew Hall <ryzngard@gmail.com>
2015-05-03 13:17:41 -0700

    Add Entity structure and Movement

---
+++ src/Entities/Chess/Pawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessDemo.Entities.Chess
+{
+    class Pawn : Entity
+    {
+        public Pawn()
+        {
+            this.MoveDistance = 1;
+            this.MoveBehavior.MoveType = MoveBehavior.MoveTypes.Forward;
+        }
+    }
+}
---
+++ src/Entities/Entity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChessDemo.Entities
+{
+    class Entity
+    {
+        /// <summary>
+        /// Current location of the Entity
+        /// </summary>
+        public Point Position;
+
+        /// <summary>
+        /// Sprite to draw
+        /// </summary>
+        public Texture2D EntityTexture;
+
+        protected MoveBehavior MoveBehavior;
+
+        public int MoveDistance
+        {
+            get
+            {
+                return MoveBehavior.MoveDistance;
+            }
+            set 
+            {
+                MoveBehavior.MoveDistance = value;
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of points, relative to the current position of the entity, that 
+        /// can be moved to.
+        /// </summary>
+        /// <returns></returns>
+        public Vector2[] GetAvailableMovements()
+        {
+            return MoveBehavior.GetAvailableMovements();
+        }
+
+        //TODO: Other Non-Chess ones, such as entities that can 'attack'
+    }
+}
